a1e3c81f0277d1a7df6cc4bcca4d24fe4bd64b4d
Rachel Poulos <rpoulos@insiten.com>
2021-05-18 08:38:50 -0400

    text component

--- app/components/AppText.tsx
+++ app/components/AppText.tsx
@@ -18,7 +18,16 @@ function AppText({ children }: { children: string}) {
 const styles = StyleSheet.create({
     text: {
         fontSize: 18,
-        fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+        ...Platform.select({
+            ios: {
+                fontSize: 20,
+                fontFamily: "Avenir",
+            },
+            android: {
+                fontSize: 18,
+                fontFamily: "Roboto"
+            }
+        }),
     }
 });
 
